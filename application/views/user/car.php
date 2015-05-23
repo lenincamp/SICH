0b8fed8cfbc8af4336b40572973acc2573c785db
lenincamp <lenin@MacBook-Air-de-Lenin.local>
2015-05-22 22:45:45 -0500

    sd
    ds

--- application/views/user/car.php
+++ application/views/user/car.php
@@ -96,7 +96,7 @@
 			  <!-- END CREAR MODELO -->
 			  
 			  <!-- BUSCAR MODELO -->
-			  <div class="panel panel-primary">
+			  <!--div class="panel panel-primary">
 				<div class="panel-heading" role="tab" id="headingTwo">
 				  <h4 class="panel-title">
 					<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
@@ -109,7 +109,7 @@
 					Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
 				  </div>
 				</div>
-			  </div>
+			  </div-->
 			  <!-- END BUSCAR MODELO -->
 			  
 			  <!-- LISTAR MODELOS -->
